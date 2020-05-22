f024391309d6a79d5e73a92659041b36f6dd18b3
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-05-22 10:19:08 -0400

    Fixup: pvc name for conda-store deployment

--- modules/kubernetes/services/conda-store/main.tf
+++ modules/kubernetes/services/conda-store/main.tf
@@ -126,7 +126,7 @@ resource "kubernetes_deployment" "main" {
         volume {
           name = "nfs-export-fast"
           persistent_volume_claim {
-            claim_name = "${var.name}-nfs-storage"
+            claim_name = kubernetes_persistent_volume_claim.main.metadata.0.name
           }
         }
 
