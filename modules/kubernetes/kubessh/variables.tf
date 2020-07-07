e982591699fdeb121ea245f4a7a0a38a07905143
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-07-07 14:56:10 +0530

    added kubessh helm chart

---
+++ modules/kubernetes/kubessh/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  description = "Name for helm chart kubessh deployment"
+  type        = string
+  default     = "kubessh"
+}
+
+variable "namespace" {
+  description = "Namespace for kubessh deployment"
+  type        = string
+}
+
+variable "external_endpoint" {
+  description = "External endpoint of kubessh exposed"
+  type        = string
+}
+
+variable "overrides" {
+  description = "kubessh helm chart list of overrides"
+  type        = list(string)
+  default     = []
+}
+
+variable "dependencies" {
+  description = "A list of module dependencies to be injected in kubessh"
+  type        = list(any)
+  default     = []
+}
