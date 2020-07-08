c4ee00e5fddb062bb54fae7e4469ca470a1124cf
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-07-08 19:28:39 -0400

    Outputting public key and service port

--- modules/kubernetes/kubessh/outputs.tf
+++ modules/kubernetes/kubessh/outputs.tf
@@ -1,7 +1,14 @@
-output "config" {
-  description = "kubessh configuration"
+output "public_key" {
+  description = "Public key to verify that kubessh connection"
+  value       = tls_private_key.kubessh_private_key.public_key_pem
+}
+
+output "service" {
+  description = "Service needed for nginx-ingress tcp proxy"
   value = {
-    "public-address" = var.external_endpoint
+    port      = 22
+    name      = var.name
+    namespace = var.namespace
   }
 }
 
