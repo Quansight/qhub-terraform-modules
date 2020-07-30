045d3145aee68e26d9b8543775e732c9cf75b48d
Amit Kumar <dtu.amit@gmail.com>
2020-07-30 01:33:30 +0100

    Return Jupyterhub token as output from QHub module

--- modules/kubernetes/services/meta/qhub/output.tf
+++ modules/kubernetes/services/meta/qhub/output.tf
@@ -1,3 +1,8 @@
 output "depended_on" {
   value = "${null_resource.dependency_setter.id}-${timestamp()}"
 }
+
+output "jupyterhub_api_token" {
+  description = "API token to enable in jupyterhub server"
+  value       = module.kubernetes-dask-gateway.jupyterhub_api_token
+}
