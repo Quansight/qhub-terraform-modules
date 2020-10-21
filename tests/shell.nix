881bdb67f5d2c362d077a1811a460ae823188a00
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-10-21 13:27:49 -0400

    Fixes to make minikube latest work with nix

--- tests/shell.nix
+++ tests/shell.nix
@@ -1,15 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  newerPkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6449c5eee5ef929a8a2f1ac30699a50907e1a2ed.tar.gz";
-    sha256 = "0h4cjghqlp44lyrdzv80bdi7vvi6j9na3cjaqs9jvb9p0rsn6026";
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/c4b26e702044dbf40f8236136c099d8ab6778514.tar.gz";
+    sha256 = "0w6hgs01qzni3a7cvgadjlmcdlb6vay3w910vh4k9fc949ii7s60";
   }) { };
 in
 pkgs.mkShell {
   buildInputs = [
-    newerPkgs.terraform_0_13
+    pkgs.terraform_0_13
     pkgs.minikube
+    pkgs.docker-machine
+    pkgs.docker-machine-kvm2
     pkgs.kubectl
     pkgs.k9s
   ];
