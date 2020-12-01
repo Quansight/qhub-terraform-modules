b2ba6a35f59778231ea22118d2890b67e5a8b41a
Brian Larsen <B_R_L@hotmail.com>
2020-12-01 09:32:31 -0600

    Add api version

--- modules/local/kubernetes/main.tf
+++ modules/local/kubernetes/main.tf
@@ -2,6 +2,7 @@ resource "kind_cluster" "main" {
     name = var.name
     kind_config  {
         kind = "Cluster"
+        api_version = "kind.x-k8s.io/v1alpha4"
         node {
             role = "control-plane"
         }
