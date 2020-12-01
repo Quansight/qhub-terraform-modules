782239dbd432659117bdfb2c92c7c7ef61869d7f
Brian Larsen <B_R_L@hotmail.com>
2020-12-01 09:25:48 -0600

    Add multiple nodes

--- modules/local/kubernetes/main.tf
+++ modules/local/kubernetes/main.tf
@@ -1,3 +1,15 @@
 resource "kind_cluster" "main" {
     name = var.name
+    kind_config  {
+        kind = "Cluster"
+        node {
+            role = "control-plane"
+        }
+        node {
+            role =  "worker"
+        }
+        node {
+            role =  "worker"
+        }
+    }
 }
