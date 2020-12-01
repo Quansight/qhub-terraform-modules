3182369216bc07f93907221dc7f94717e228c007
Brian Larsen <B_R_L@hotmail.com>
2020-12-01 13:25:12 -0600

    Don't decode cert

--- modules/local/kubernetes/outputs.tf
+++ modules/local/kubernetes/outputs.tf
@@ -4,7 +4,7 @@ output "credentials" {
     endpoint = kind_cluster.main.endpoint
     client_certificate = kind_cluster.main.client_certificate
     client_key = kind_cluster.main.client_key
-    cluster_ca_certificate = base64decode(kind_cluster.main.cluster_ca_certificate)
+    cluster_ca_certificate = kind_cluster.main.cluster_ca_certificate
     kubeconfig = kind_cluster.main.kubeconfig
     kubeconfig_path = kind_cluster.main.kubeconfig_path
   }
