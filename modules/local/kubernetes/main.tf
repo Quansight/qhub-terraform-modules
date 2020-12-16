ddc39e724cf0af5bc8db74819913f556a4145b6e
Brian Larsen <B_R_L@hotmail.com>
2020-12-16 10:04:15 -0600

    Update to latest kind image

--- modules/local/kubernetes/main.tf
+++ modules/local/kubernetes/main.tf
@@ -1,7 +1,7 @@
 resource "kind_cluster" "main" {
   name           = var.name
   wait_for_ready = true
-  node_image     = "kindest/node:v1.18.8@sha256:f4bcc97a0ad6e7abaf3f643d890add7efe6ee4ab90baeb374b4f41a4c95567eb"
+  node_image     = "kindest/node:v1.19.1@sha256:98cf5288864662e37115e362b23e4369c8c4a408f99cbc06e58ac30ddc721600"
   kind_config {
     kind        = "Cluster"
     api_version = "kind.x-k8s.io/v1alpha4"
