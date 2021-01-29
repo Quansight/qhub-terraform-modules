f26e9421d47c3c0802c8c9be1173d4a0619a3bdd
Amit Kumar <dtu.amit@gmail.com>
2021-01-29 12:04:25 +0000

    add prefect variables

--- modules/kubernetes/services/prefect/variables.tf
+++ modules/kubernetes/services/prefect/variables.tf
@@ -3,6 +3,16 @@ variable "jupyterhub_api_token" {
   default = ""
 }
 
+variable "namespace" {
+  type    = string
+  default = ""
+}
+
+variable "prefect_token" {
+  type    = string
+  default = ""
+}
+
 variable "dependencies" {
   description = "A list of module dependencies to be injected in the module"
   type        = list(any)
