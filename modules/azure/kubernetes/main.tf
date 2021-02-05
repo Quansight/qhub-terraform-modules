844dc16fc9d332443d0ca49fa5e97b61b16dd741
Adam Lewis <balast@users.noreply.github.com>
2021-02-05 16:16:21 -0600

    terraform format

--- modules/azure/kubernetes/main.tf
+++ modules/azure/kubernetes/main.tf
@@ -1,5 +1,5 @@
 provider azurerm {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "qhub_resource_group" {
@@ -12,27 +12,27 @@ resource "azurerm_kubernetes_cluster" "main" {
   name                = var.name
   location            = azurerm_resource_group.qhub_resource_group.location
   resource_group_name = azurerm_resource_group.qhub_resource_group.name
-  
+
   # DNS prefix specified when creating the managed cluster. Changing this forces a new resource to be created.
-  dns_prefix          = "Qhub"  # required
-  
+  dns_prefix = "Qhub" # required
+
   # Azure requires that a new, non-existent Resource Group is used, as otherwise the provisioning of the Kubernetes Service will fail.
-  node_resource_group = "node_resource_group"  # optional
+  node_resource_group = "node_resource_group" # optional
   default_node_pool {
-    name                  = "general"
-    node_count            = 1
-    vm_size               = "Standard_D2_v2"
-    enable_auto_scaling   = "true"
-    min_count             = 1
-    max_count             = 1
-    node_labels           = var.node_labels
-    orchestrator_version  = var.kubernetes_version
+    name                 = "general"
+    node_count           = 1
+    vm_size              = "Standard_D2_v2"
+    enable_auto_scaling  = "true"
+    min_count            = 1
+    max_count            = 1
+    node_labels          = var.node_labels
+    orchestrator_version = var.kubernetes_version
   }
-  
-  sku_tier = "Free"  # "Free" [Default] or "Paid"
+
+  sku_tier = "Free" # "Free" [Default] or "Paid"
 
   identity {
-    type = "SystemAssigned"  # "UserAssigned" or "SystemAssigned".  SystemAssigned identity lifecycles are tied to the AKS Cluster.
+    type = "SystemAssigned" # "UserAssigned" or "SystemAssigned".  SystemAssigned identity lifecycles are tied to the AKS Cluster.
   }
 
   tags = var.tags
@@ -46,7 +46,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
   vm_size               = var.node_pools[count.index].instance_type
   node_count            = 0
   enable_auto_scaling   = "true"
-  mode                  = "User"  # "System" or "User", only "User" nodes can scale down to 0
+  mode                  = "User" # "System" or "User", only "User" nodes can scale down to 0
   min_count             = var.node_pools[count.index].min_size
   max_count             = var.node_pools[count.index].max_size
   tags                  = var.tags
