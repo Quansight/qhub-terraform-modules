03ff79b5529bf4df657b20a26cc32eebaab1dc60
Adam Lewis <balast@users.noreply.github.com>
2021-02-09 16:03:15 -0600

    add registry

---
+++ modules/azure/registry/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "resource-group" {
+  name     = "${var.name}-container-registry"
+  location = var.location
+}
+
+resource "azurerm_container_registry" "container_registry" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  sku                 = "Standard"
+  # admin_enabled       = true
+}
