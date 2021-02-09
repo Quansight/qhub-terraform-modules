d42207c9f5226c1266b502201f3dbf34114a9b97
Adam Lewis <balast@users.noreply.github.com>
2021-02-09 16:03:15 -0600

    add registry

---
+++ modules/azure/registry/variable.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Prefix name to git container registry"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of container registry resource group"
+  type        = string
+}
