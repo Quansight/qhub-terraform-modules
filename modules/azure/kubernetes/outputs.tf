bfb537c2393a15adfc0516230cf8fc8c97870ea5
Adam Lewis <balast@users.noreply.github.com>
2021-02-10 16:51:49 -0600

    rename password to token

--- modules/azure/kubernetes/outputs.tf
+++ modules/azure/kubernetes/outputs.tf
@@ -5,7 +5,7 @@ output "credentials" {
     # see bottom of https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
     endpoint               = azurerm_kubernetes_cluster.main.kube_config.0.host
     username               = azurerm_kubernetes_cluster.main.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.main.kube_config.0.password
+    token                  = azurerm_kubernetes_cluster.main.kube_config.0.password
     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
