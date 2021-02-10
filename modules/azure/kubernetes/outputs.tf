f3b663e8796292e1bf73a638d801c07afa2c9e75
Adam Lewis <balast@users.noreply.github.com>
2021-02-10 08:40:20 -0600

    make azure credential outputs consistent with other cloud providers

--- modules/azure/kubernetes/outputs.tf
+++ modules/azure/kubernetes/outputs.tf
@@ -3,7 +3,7 @@ output "credentials" {
   description = "Credentials required for connecting to kubernetes cluster"
   value = {
     # see bottom of https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
-    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
+    endpoint               = azurerm_kubernetes_cluster.main.kube_config.0.host
     username               = azurerm_kubernetes_cluster.main.kube_config.0.username
     password               = azurerm_kubernetes_cluster.main.kube_config.0.password
     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
