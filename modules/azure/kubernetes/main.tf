20b68594311f48b12984d1ef68ca8be8acef5d89
Adam Lewis <balast@users.noreply.github.com>
2021-02-11 17:14:47 -0600

    node_labesl

--- modules/azure/kubernetes/main.tf
+++ modules/azure/kubernetes/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     max_count            = 1
     # node_labels          = var.node_labels
     # orchestrator_version = var.kubernetes_version
-    tags = {
+    node_labels          = {
       "azure-node-pool" = "general"
     }
   }
@@ -52,7 +52,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "node_groups" {
   mode                  = "User" # "System" or "User", only "User" nodes can scale down to 0
   min_count             = var.node_groups[count.index].min_size
   max_count             = var.node_groups[count.index].max_size
-  node_labels                  = {
+  node_labels           = {
       "azure-node-pool" = var.node_groups[count.index].name
     }
   # orchestrator_version  = var.kubernetes_version
