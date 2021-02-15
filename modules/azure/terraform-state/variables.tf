591b81efcb105926562e073cf7daf64ea5c931a7
Adam Lewis <balast@users.noreply.github.com>
2021-02-15 15:15:42 -0600

    add storage account postfix

--- modules/azure/terraform-state/variables.tf
+++ modules/azure/terraform-state/variables.tf
@@ -7,3 +7,8 @@ variable "location" {
   description = "Location for terraform state"
   type        = string
 }
+
+variable "storage_account_postfix" {
+  description = "random characters appended to storage account name to facilitate global uniqueness"
+  type        = string
+}
