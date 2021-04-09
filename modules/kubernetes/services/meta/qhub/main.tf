e9efefba80d6f0a46ff4441cfad559580f4ff8a0
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-04-09 11:47:58 -0400

    Adding default cluster options

--- modules/kubernetes/services/meta/qhub/main.tf
+++ modules/kubernetes/services/meta/qhub/main.tf
@@ -102,6 +102,61 @@ module "kubernetes-dask-gateway" {
   general-node-group = var.general-node-group
   worker-node-group  = var.worker-node-group
 
+  # default cluster behavior
+  cluster = {
+    # scheduler configuration
+    scheduler_cores        = 1
+    scheduler_cores_limit  = 1
+    scheduler_memory       = "2 G"
+    scheduler_memory_limit = "2 G"
+    scheduler_extra_container_config = {
+      volumeMounts = [
+        {
+          name      = "conda-store"
+          mountPath = "/home/conda"
+        }
+      ]
+    }
+    scheduler_extra_pod_config = {
+      affinity = local.affinity.worker-nodegroup
+      volumes = [
+        {
+          name = "conda-store"
+          persistentVolumeClaim = {
+            claimName = var.conda-store-pvc
+          }
+        }
+      ]
+    }
+    # worker configuration
+    worker_cores        = 1
+    worker_cores_limit  = 1
+    worker_memory       = "2 G"
+    worker_memory_limit = "2 G"
+    worker_extra_container_config = {
+      volumeMounts = [
+        {
+          name      = "conda-store"
+          mountPath = "/home/conda"
+        }
+      ]
+    }
+    worker_extra_pod_config = {
+      affinity = local.affinity.worker-nodegroup
+      volumes = [
+        {
+          name = "conda-store"
+          persistentVolumeClaim = {
+            claimName = var.conda-store-pvc
+          }
+        }
+      ]
+    }
+    # additional fields
+    image_pull_policy = "IfNotPresent"
+    environment       = {}
+  }
+
   extra_config = var.dask_gateway_extra_config
 }
 
