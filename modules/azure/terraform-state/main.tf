40b9d11f58c73090d54a3803fd0c0edba9e83dd2
Adam Lewis <balast@users.noreply.github.com>
2021-04-12 11:49:30 -0500

    resource group

--- modules/azure/terraform-state/main.tf
+++ modules/azure/terraform-state/main.tf
@@ -3,12 +3,12 @@ provider azurerm {
 }
 
 resource "azurerm_resource_group" "terraform-resource-group" {
-  name     = var.name
+  name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_storage_account" "terraform-storage-account" {
-  name                     = "${var.name}${var.storage_account_postfix}" # must be unique across the entire Azure service
+  name                     = "${var.resource_group_name}${var.storage_account_postfix}" # must be unique across the entire Azure service
   location                 = azurerm_resource_group.terraform-resource-group.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "terraform-storage-account" {
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name                  = "${var.name}state"
+  name                  = "${var.resource_group_name}state"
   storage_account_name  = azurerm_storage_account.terraform-storage-account.name
   container_access_type = "private"
 }
