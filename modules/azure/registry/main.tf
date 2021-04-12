f3ae46643c9c8caee894ad4ccc669e3b0b628203
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-04-12 17:28:10 -0400

    Terraform formatting

--- modules/azure/registry/main.tf
+++ modules/azure/registry/main.tf
@@ -9,7 +9,7 @@ provider azurerm {
 resource "azurerm_container_registry" "container_registry" {
   name                = var.name
   resource_group_name = var.resource_group_name #azurerm_resource_group.resource-group.name
-  location            = var.location #azurerm_resource_group.resource-group.location
+  location            = var.location            #azurerm_resource_group.resource-group.location
   sku                 = "Standard"
   # admin_enabled       = true
 }
