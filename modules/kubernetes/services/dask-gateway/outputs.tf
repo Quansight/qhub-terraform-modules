30a27f9c3a12884f57258a39083b0e9b320382a8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-04-12 08:35:32 -0400

    Expose public service url due to self-signed issue

--- modules/kubernetes/services/dask-gateway/outputs.tf
+++ modules/kubernetes/services/dask-gateway/outputs.tf
@@ -2,8 +2,9 @@ output "config" {
   description = "dask gateway /etc/dask/dask-gateway.yaml configuration"
   value = {
     gateway = {
-      address       = "http://${kubernetes_service.gateway.metadata.0.name}.${kubernetes_service.gateway.metadata.0.namespace}:8000"
-      proxy_address = "tcp://${var.external-url}:8786"
+      address        = "http://${kubernetes_service.gateway.metadata.0.name}.${kubernetes_service.gateway.metadata.0.namespace}:8000"
+      public_address = "https://${var.external-url}/gateway"
+      proxy_address  = "tcp://${var.external-url}:8786"
 
       auth = {
         type = "jupyterhub"
